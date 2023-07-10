769fc1ba8b10d3eb963a993444af5c4420d38eba
Денис <garaevdenis2019@gmail.com>
2023-07-10 22:17:15 +0500

    Initiate commit

--- src/main/java/com/exam/examspring/controller/QuestionController.java
+++ src/main/java/com/exam/examspring/controller/QuestionController.java
@@ -1,10 +1,13 @@
 package com.exam.examspring.controller;
 
+import com.exam.examspring.Question;
 import com.exam.examspring.interfaces.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
 
 @RestController
 @RequestMapping("/exam/java")
@@ -15,5 +18,24 @@ public class QuestionController {
         this.questionService = questionService;
     }
     @GetMapping("/add")
+    Question add(String question, String answer) {
+        return questionService.add(question, answer);
+    }
+    @GetMapping("/add")
+    Question add(Question question) {
+        return questionService.add(question);
+    }
+    @GetMapping("/remove")
+    Question remove(Question question) {
+        return questionService.remove(question);
+    }
+    @GetMapping("/getAll")
+    Collection<Question> getAll() {
+        return questionService.getAll();
+    }
+    @GetMapping("/getRandomQuestion")
+    Question getRandomQuestion() {
+        return questionService.getRandomQuestion();
+    }
 
 }
